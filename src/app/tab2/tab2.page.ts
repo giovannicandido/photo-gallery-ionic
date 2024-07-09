9074743965f004fb3cdb6d28432422e4954c4fb3
Giovanni Silva <git@gsilva.pro>
2024-07-09 20:48:02 -0300

    Initial Commit Form Com modal

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ViewDidEnter } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit, ViewDidEnter {
 
   constructor() {}
+
+  ionViewDidEnter(): void {
+    console.info("Navegou para Tab 2")
+  }
+
+  ngOnInit(): void {
+    console.info("Tab 2 construida")
+  }
 
 }
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ViewDidEnter } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {
+export class Tab3Page implements OnInit, ViewDidEnter {
 
   constructor() {}
+
+  ngOnInit(): void {
+    console.info("Tab 3 construida")
+  }
+
+  ionViewDidEnter(): void {
+    console.info("Navegou para Tab 3")
+  }
 
 }
