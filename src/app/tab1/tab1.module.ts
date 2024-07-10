146c0bb8072723dabdd7b0944f4ce6fa92ab2d6b
Giovanni Silva <git@gsilva.pro>
2024-07-09 21:59:57 -0300

    pegando valores no form

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -1,17 +1,17 @@
 import { IonicModule } from '@ionic/angular';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
 import { Tab1PageRoutingModule } from './tab1-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
-    FormsModule,
+    ReactiveFormsModule,
     ExploreContainerComponentModule,
     Tab1PageRoutingModule
   ],
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { IonModal, ModalController } from '@ionic/angular';
 import { IonPickerColumnCustomEvent, PickerColumnChangeEventDetail, PickerColumnValue } from '@ionic/core';
 
@@ -12,11 +13,20 @@ export class Tab1Page {
   selectedColor: PickerColumnValue = ""
 
   message = ""
+  showMessage = false
+  toastMessage = ""
+
+  formGroup: FormGroup
 
   @ViewChild(IonModal)
   modal!: IonModal
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { 
+    this.formGroup = fb.group({
+      name: ['', Validators.required],
+      color: ['', Validators.required]
+    })
+  }
 
   done() {
     if (this.selectedColor?.toString().trim() !== "") {
@@ -31,11 +41,18 @@ export class Tab1Page {
 
   colorChanged($event: IonPickerColumnCustomEvent<PickerColumnChangeEventDetail>) {
     this.selectedColor = $event.detail.value
+    this.formGroup.controls['color'].setValue(this.selectedColor);
     this.message = ""
   }
 
   save() {
-    console.info("Saving...")
+    if(this.formGroup.valid) {
+      console.info("Saving...")
+      console.info(this.formGroup.value)
+    } else {
+      this.toastMessage = "Invalid form"
+      this.showMessage = true
+    }
   }
 
 }
