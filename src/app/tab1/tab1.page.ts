c75238f82acee352c94ce9ba3c7eb34ce1b4cdce
Giovanni Silva <git@gsilva.pro>
2024-07-09 21:28:32 -0300

    select cat name

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonModal, ModalController } from '@ionic/angular';
+import { IonPickerColumnCustomEvent, PickerColumnChangeEventDetail, PickerColumnValue } from '@ionic/core';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +9,29 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  selectedColor: PickerColumnValue = ""
+
+  message = ""
+
+  @ViewChild(IonModal)
+  modal!: IonModal
+
+  constructor() { }
+
+  done() {
+    if (this.selectedColor?.toString().trim() !== "") {
+      console.info("Done: " + this.selectedColor)
+      this.message = ""
+      this.modal.dismiss()
+    } else {
+      this.message = "Please select a color"
+    }
+
+  }
+
+  colorChanged($event: IonPickerColumnCustomEvent<PickerColumnChangeEventDetail>) {
+    this.selectedColor = $event.detail.value
+    this.message = ""
+  }
 
 }
