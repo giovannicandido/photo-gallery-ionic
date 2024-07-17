64284a97592dd10ab534dd04ef73fe954eca3650
Giovanni Silva <git@gsilva.pro>
2024-07-16 22:00:04 -0300

    inicializado database e criado servico de persistencia

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,17 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IonicStorageModule } from '@ionic/storage-angular';
+import { Drivers } from '@ionic/storage'
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
+    IonicStorageModule.forRoot({
+      name: "photo-galery",
+      driverOrder: [Drivers.IndexedDB, Drivers.LocalStorage]
+    })
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/app/services/database-service.ts
+++ src/app/services/database-service.ts
@@ -1,33 +1,55 @@
 import { Injectable } from "@angular/core";
+import { Storage } from "@ionic/storage-angular";
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class DataBaseService {
-    
-    /**
-     * Save any element in database
-     * @param key A ID representing the key of the value
-     * @param value Value to save in database
-     * @param autoIncrement If true will cancatenate an autoIncrement value to the key
-     *  if the value has the key the autoIncrement will not create a new one even if true.
-     *  For example: saveOrUpdate('cat', {name: "Name"}) -> creates the cat-1 key
-     *  saveOrUpdate('cat-1', {name: "Name 2"}) -> updates cat-1 key (it will not create a new one)
-     */
-    saveOrUpdate(key: string, value: any, autoIncrement = false) {
+  private _initStorage: Storage | null = null
 
-    }
+  constructor(private storage: Storage) {
+    this.init()
+  }
 
-    getByKey(key: string): any {
-        return {}
-    }
+  async init() {
+    // sem await
+    //const storage = this.storage.create()
+    // promise
+    //this.storage.create()
+    //  .then(storageCriado => this._storage = storageCriado)
+    const storage = await this.storage.create()
+    this._initStorage = storage;
+  }
 
-    findAllByKey(key: string): any[] {
-        return [{
-            name: 'Mila',
-            color: "white"
-          }, {
-            name: 'Mel',
-            color: 'yellow'
-          }]
+  /**
+   * Save any element in database
+   * @param key A ID representing the key of the value
+   * @param value Value to save in database
+   * @param autoIncrement If true will cancatenate an autoIncrement value to the key
+   *  if the value has the key the autoIncrement will not create a new one even if true.
+   *  For example: saveOrUpdate('cat', {name: "Name"}) -> creates the cat-1 key
+   *  saveOrUpdate('cat-1', {name: "Name 2"}) -> updates cat-1 key (it will not create a new one)
+   */
+  async saveOrUpdate(key: string, value: any, autoIncrement = false) {
+    if (autoIncrement) {
+      const all = await this.findAllByKey(key)
+      all.push(value)
+      this._initStorage?.set(key, all)
+    } else {
+      // if false
+      this._initStorage?.set(key, [value])
     }
+  }
+
+  getByKey(key: string): any {
+    return this._initStorage?.get(key)
+  }
+
+  async findAllByKey(key: string): Promise<any[]> {
+    const value = await this._initStorage?.get(key)
+    if(value === null || value === undefined) {
+      return []
+    }
+    const values = value as any[]
+    return values
+  }
 
 }
